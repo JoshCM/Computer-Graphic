11833fe6517f36e320b7d382d51161ba68168615
Joshua Mestre <joshua.mestre@outlook.com>
2018-06-11 12:13:58 +0200

    finished product

--- Blatt7/openGLAbgabe.py
+++ Blatt7/openGLAbgabe.py
@@ -16,33 +16,42 @@ FACES = []
 VERTICIES = []
 NORM = []
 POINTS = []
+MITTELPUNKT = None
 VBO = "Hallo"
 ZOOM = 0
 rotStartP = 0
+transStartP = 0
 actOri = np.matrix([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
 angle = 0
 doRotation = False
 doTranslate = False
-xtrans = ytrans = ztrans = 0
+xtrans = ytrans = 0
 scaleFactor = 0
 axis = np.array([.0,.0,.0])
 ortho = True
 ObjectColor = (1.0,1.0,1.0,1.0)
+lightPos = [0.0,1000.0,100.0]
+shadow = False
+objMin = []
 
 
 def init(width, height):
-    global ObjectColor
+    global ObjectColor,lightPos
     """ Initialize an OpenGL window """
-    glClearColor(0.0, 0.0, 0.0, 1.0)  # background color
+    glClearColor(0.0, 0.0, 1.0, 1.0)  # background color
     glEnable(GL_DEPTH_TEST)
     glEnable(GL_NORMALIZE)
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0,GL_POSITION,lightPos)
+    
+    glEnable(GL_CULL_FACE)
+    glPolygonMode(GL_FRONT, GL_LINE)
 
     glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,ObjectColor)
     glMatrixMode(GL_PROJECTION)  # switch to projection matrix
     glLoadIdentity()  # set to 1
-    glOrtho(-1.5, 1.5, -1.5, 1.5, -1.0, 1.0)  # multiply with new p-matrix
+    glOrtho(-1.5, 1.5, -1.5, 1.5, 0.1, 100.0)  # multiply with new p-matrix
     glMatrixMode(GL_MODELVIEW)  # switch to modelview matrix
 
 def rotate(angle,axis):
@@ -57,37 +66,55 @@ def rotate(angle,axis):
 
 def display():
     """ Render all objects"""
-    global VBO,scaleFactor,axis,angle,doRotation,ortho,xtrans,ytrans,ztrans
+    global VBO,MITTELPUNKT,scaleFactor,axis,angle,doRotation,ortho,xtrans,ytrans,ztrans,shadow,lightPos
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # clear screen
-    glColor(0.0, 0.0, 0.0)  # render stuff
     glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,ObjectColor)
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
-    objMax = [max(POINTS, key=lambda x: x[0])[0], max(POINTS, key=lambda x: x[1])[1], max(POINTS, key=lambda x: x[2])[2]]
-    objMin = [min(POINTS, key=lambda x: x[0])[0], min(POINTS, key=lambda x: x[1])[1], min(POINTS, key=lambda x: x[2])[2]]
-    MITTELPUNKT = [(mi+ma)/2.0 for mi, ma in zip(objMax, objMin)]
-    scaleFactor = 2/max([(ma-mi) for ma, mi in zip(objMax, objMin)])
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glTranslate(float(xtrans)/WIDTH,float(ytrans)/WIDTH,0)
-    if ortho:
-        glOrtho(-1.5, 1.5, -1.5, 1.5, -1.0, 1.0)
-    else:
-        gluPerspective(45,float(WIDTH)/HEIGHT,0.1,100)
-        gluLookAt(0,0,4,0,0,0,0,1,0)
-    glMatrixMode(GL_MODELVIEW)
-
+    #glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+    
     VBO.bind()
     glEnableClientState(GL_VERTEX_ARRAY)
     glEnableClientState(GL_NORMAL_ARRAY)
-    glVertexPointer(3, GL_FLOAT, 24, VBO)
-    glNormalPointer(GL_FLOAT, 24, VBO+12)
+
+    if shadow:
+        shadowMatrix = [1.0, 0, 0, 0, 0, 1.0, 0, -1.0/lightPos[1], 0, 0, 1.0, 0, 0, 0, 0, 0]
+
+        glPushMatrix() # save state
+        glTranslatef(0.0,objMin[1],0.0)#basis des objects auf der y ebene
+        glTranslatef(lightPos[0],lightPos[1],lightPos[2]) # translate back
+        glMultMatrixf(shadowMatrix) # project object
+        glTranslatef(-lightPos[0], -lightPos[1], -lightPos[2]) # move light to origin
+        glTranslatef(0.0,-objMin[1],0.0)#basis des objects auf der y ebene
+        glColor3fv([0.0,0.0,0.0])
+        glDisable(GL_LIGHTING)
+        glDisable(GL_DEPTH_TEST)
+        glDrawArrays(GL_TRIANGLES, 0, len(POINTS))
+        glEnable(GL_LIGHTING)
+        glEnable(GL_DEPTH_TEST)
+        glPopMatrix()
+        glVertexPointer(3, GL_FLOAT, 24, VBO)
+        glNormalPointer(GL_FLOAT, 24, VBO+12)
+        
+
+
+    # Set camera
     glLoadIdentity()
+    gluLookAt(0,0,4, 0,0,0, 0,1,0)
+
+    # Interaction
+    glTranslate(float(xtrans)/(WIDTH/2.0),float(ytrans)/(HEIGHT/2.0),0)
     glMultMatrixf((actOri*rotate(angle,axis)).tolist())
+    
+    # Normilization
     glScale(scaleFactor, scaleFactor, scaleFactor)
     glTranslate(-MITTELPUNKT[0], -MITTELPUNKT[1], -MITTELPUNKT[2])
+    
+    # call OpenGL draw
+    glVertexPointer(3, GL_FLOAT, 24, VBO)
+    glNormalPointer(GL_FLOAT, 24, VBO+12)
     glDrawArrays(GL_TRIANGLES, 0, len(POINTS))
+
     VBO.unbind()
     glDisableClientState(GL_VERTEX_ARRAY)
     glDisableClientState(GL_NORMAL_ARRAY)
@@ -103,28 +130,34 @@ def reshape(width, height):
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    if width <= height:
-        glOrtho(-1.5, 1.5,
-                -1.5*height/width, 1.5*height/width,
-                -1.0, 1.0)
+    if ortho: 
+        if width <= height:
+            glOrtho(-1.5, 1.5,
+                    -1.5*height/width, 1.5*height/width,
+                    0.1, 100.0)
+        else:
+            glOrtho(-1.5*width/height, 1.5*width/height,
+                    -1.5, 1.5,
+                    0.1, 100.0)
+
     else:
-        glOrtho(-1.5*width/height, 1.5*width/height,
-                -1.5, 1.5,
-                -1.0, 1.0)
+        gluPerspective(45,float(WIDTH)/HEIGHT,0.1,100)
+   
     glMatrixMode(GL_MODELVIEW)
-
-
+    
 def keyPressed(key, x, y):
     """ handle keypress events """
-    global ortho,ObjectColor
+    global ortho,ObjectColor,scaleFactor,shadow
 
     if key == chr(27):  # chr(27) = ESCAPE
         sys.exit()
 
-    if key == "O":
+    if key == "o":
         ortho = True
-    elif key == "P":
+        reshape(WIDTH, HEIGHT)
+    elif key == "p":
         ortho = False
+        reshape(WIDTH, HEIGHT)
     elif key == "S":
         glClearColor(0.0,0.0,0.0,1.0)
     elif key == "s":
@@ -145,10 +178,18 @@ def keyPressed(key, x, y):
         glClearColor(0.0,0.0,1.0,1.0)
     elif key == "b":
         ObjectColor = (0.0,0.0,1.0,1.0)
+    elif key == "z":
+        scaleFactor *= 1.1
+    elif key == "Z":
+        scaleFactor *= 0.9
+    elif key == "h":
+        shadow = not shadow
+
     glutPostRedisplay()
 
 def projectOnSpher(x,y,r):
-    global WIDTH, HEIGHT
+    global WIDTH, HEIGHT,xtrans,ytrans
+
     x,y = x-WIDTH/2.0, HEIGHT/2.0-y
     a = min(r*r,x**2+y**2)
     z = math.sqrt(r*r -a)
@@ -156,9 +197,9 @@ def projectOnSpher(x,y,r):
     return np.array([x/l, y/l, z/l])
 
 def mouse(button, state, x, y):
-    global rotStartP, actOri, angle, doRotation,doTranslate
+    global rotStartP, actOri, angle, doRotation,doTranslate,transStartP
     r = min(WIDTH,HEIGHT)/2.0
-    if button == GLUT_MIDDLE_BUTTON:
+    if button == GLUT_LEFT_BUTTON:
         if state == GLUT_DOWN:
             doRotation = True
             rotStartP = projectOnSpher(x,y,r)
@@ -169,14 +210,13 @@ def mouse(button, state, x, y):
     if button == GLUT_RIGHT_BUTTON:
         if state == GLUT_DOWN:
             doTranslate = True
+            transStartP = np.array([x-WIDTH/2.0, HEIGHT/2.0-y])
         if state == GLUT_UP:
             doTranslate = False
 
-
-
 def mouseMotion(x, y):
     """ handle mouse motion """
-    global angle, axis,scaleFactor,rotStartP,doTranslate,xtrans,ytrans
+    global angle, axis,scaleFactor,rotStartP,doTranslate,xtrans,ytrans,transStartP
     if doRotation:
         r = min(WIDTH,HEIGHT)/2.0
         moveP = projectOnSpher(x,y,r)
@@ -188,10 +228,9 @@ def mouseMotion(x, y):
         angle = math.acos(dot)
         axis = np.cross(rotStartP,moveP)
     if doTranslate:
-        xtrans = x-WIDTH/2
-        ytrans = HEIGHT/2-y
+        xtrans = transStartP[0]+(x-WIDTH/2.0)
+        ytrans = transStartP[1]+(HEIGHT/2.0-y)
     glutPostRedisplay()  
-
 
 def menu_func(value):
     """ handle menue selection """
@@ -200,10 +239,42 @@ def menu_func(value):
         sys.exit()
     glutPostRedisplay()
 
+def calcNorms():
+    global FACES, VERTICIES,NORM
+
+    for i in range(len(VERTICIES)):
+        NORM.append(np.array([0,0,0],'f'))
+
+    for ele in FACES:
+        index = [int(ele[0].split("/")[0])-1,int(ele[1].split("/")[0])-1,int(ele[2].split("/")[0])-1]
+        vert1 = np.array(VERTICIES[index[0]],'f')
+        vert2 = np.array(VERTICIES[index[1]],'f')
+        vert3 = np.array(VERTICIES[index[2]],'f')
+
+        vec1 = (vert2-vert1)
+        vec2 = (vert3-vert1)
+
+        norm = np.cross(vec1,vec2)
+
+        for i in index:
+            NORM[i] += norm
+    
+    for i in range(len(NORM)):
+        ele = NORM[i]
+        norm = np.linalg.norm(ele)
+        if norm != 0.0:
+            NORM[i] = (ele/norm).tolist()
+        else:
+            ele = [0.0,0.0,0.0]
+    
+
+
+
+
 
 def main():
-    global VBO
-    with open('benjamin.obj') as file:
+    global VBO,MITTELPUNKT,scaleFactor,NORM,objMin
+    with open(sys.argv[1]) as file:
         for line in file.readlines():
             if line.startswith("vn"):
                 vn = [float(x) for x in line.split()[1:]]
@@ -215,10 +286,22 @@ def main():
                 f = [x for x in line.split()[1:]]
                 FACES.append(f)
 
+    calcedNorm = False
+    if len(NORM) == 0:
+        calcNorms()
+        calcedNorm = True
+    
     for ele in FACES:
         for e in ele:
-            POINTS.append(
-                VERTICIES[int(e.split("/")[0])-1] + NORM[int(e.split("/")[2])-1])
+            if calcedNorm:
+                POINTS.append(VERTICIES[int(e.split("/")[0])-1] + NORM[int(e.split("/")[0])-1])
+            else:
+                POINTS.append(VERTICIES[int(e.split("/")[0])-1] + NORM[int(e.split("/")[2])-1])
+
+    objMax = [max(POINTS, key=lambda x: x[0])[0], max(POINTS, key=lambda x: x[1])[1], max(POINTS, key=lambda x: x[2])[2]]
+    objMin = [min(POINTS, key=lambda x: x[0])[0], min(POINTS, key=lambda x: x[1])[1], min(POINTS, key=lambda x: x[2])[2]]
+    MITTELPUNKT = [(mi+ma)/2.0 for mi, ma in zip(objMax, objMin)]
+    scaleFactor = 2/max([(ma-mi) for ma, mi in zip(objMax, objMin)])
 
     VBO = vbo.VBO(np.array(POINTS, 'f'))
 
@@ -227,7 +310,7 @@ def main():
     glutInit(sys.argv)
     os.chdir(cwd)
 
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB|GLUT_DEPTH)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutInitWindowSize(WIDTH, HEIGHT)
     glutCreateWindow("beschissener Kack")
 
